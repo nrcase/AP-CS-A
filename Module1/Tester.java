f2b8c391174d1b66c8021a2d73708e8bccb167ac
Nick Case <55997556+nrcase@users.noreply.github.com>
2020-03-08 20:01:58 -0400

    Add files via upload

---
+++ Module1/Tester.java
@@ -0,0 +1,11 @@
+/*Author: Nick Case
+Date: September 2nd, 2019
+School: CHHS*/
+public class Tester
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        
+    }
+}
